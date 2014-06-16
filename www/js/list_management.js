9afe0b972ab5a24a98f83d596685053d937c8a3c
angelalonso <alonsofonseca.angel@gmail.com>
2014-06-16 19:06:28 +0200

    new colors #3

---
+++ www/js/list_management.js
@@ -0,0 +1,53 @@
+function show_list_entries(list_2show) {
+	if (localStorage.length == 0) {
+		var liTag = document.createElement("li");
+		liTag.id = "list_entry_noentries";
+        liTag.setAttribute("onclick", 'update_list_entries(' + list_2show + ');');
+        liTag.innerHTML = "No entries found";
+        document.getElementById(list_2show).appendChild(liTag);
+	} else {
+		for ( i = 0 ; i <= localStorage.length - 1 ; i++ ){
+			var key = localStorage.key(i);
+			value = localStorage[localStorage.key(i)];
+			if (key.indexOf("data_") > -1) {
+				try {
+					value_nr = key.split("_")[1];
+					var liTag = document.createElement("li");
+					liTag.id = "list_entry";
+		            liTag.setAttribute("onclick", 'localStorage.removeItem("data_' + value_nr + '");update_list_entries("' + list_2show + '");');
+		            liTag.innerHTML = value;
+		            document.getElementById(list_2show).appendChild(liTag);
+					
+				} catch(e) {
+					alert("ERROR on show_list_entries:" + e + value);
+				}
+			}
+		} 
+		//document.getElementById('lvdata').innerHTML = data_in;
+	}
+}
+
+function clear_list_entries(list_2clear) {
+	container = document.getElementById(list_2clear);
+	li_entries = container.getElementsByTagName("li");
+	li_entries_nr = li_entries.length;
+	for (i=li_entries_nr-1;i>=0;i--) {
+		container.removeChild(li_entries[i]);
+	}
+}
+
+function update_list_entries(list_2update) {
+	clear_list_entries(list_2update);
+	show_list_entries(list_2update);
+}
+
+function manage_entrylist(list_2manage) {
+	container = document.getElementById(list_2manage);
+	li_entries = container.getElementsByTagName("li");
+	li_entries_nr = li_entries.length;
+	if (li_entries_nr == 0) {
+		show_list_entries(list_2manage);
+	} else {
+		clear_list_entries(list_2manage);
+	}
+}
--- www/js/page_data.js
+++ www/js/page_data.js
@@ -36,52 +36,7 @@ function update_html_data() {
 	//document.getElementById('lvdata').innerHTML = data_in;
 }
 
-function show_list_entries() {
-	data_in = ""
-	for ( i = 0 ; i <= localStorage.length - 1 ; i++ ){
-		var key = localStorage.key(i);
-		value = localStorage[localStorage.key(i)];
-		if (key.indexOf("data_") > -1) {
-			try {
-				value_nr = key.split("_")[1];
-				var liTag = document.createElement("li");
-				liTag.id = "list_entry";
-	            liTag.setAttribute("onclick", 'localStorage.removeItem("data_' + value_nr + '");update_list_entries();');
-	            liTag.innerHTML = value;
-	            document.getElementById('data_allentries_list').appendChild(liTag);
-				
-			} catch(e) {
-				alert("ERROR on update_html_data:" + e + value);
-			}
-		}
-	} 
-	//document.getElementById('lvdata').innerHTML = data_in;
-}
 
-function clear_list_entries() {
-	container = document.getElementById('data_allentries_list');
-	li_entries = container.getElementsByTagName("li");
-	li_entries_nr = li_entries.length;
-	for (i=li_entries_nr-1;i>=0;i--) {
-		container.removeChild(li_entries[i]);
-	}
-}
-
-function update_list_entries() {
-	clear_list_entries();
-	show_list_entries();
-}
-
-function manage_entrylist(list) {
-	container = document.getElementById('data_allentries_list');
-	li_entries = container.getElementsByTagName("li");
-	li_entries_nr = li_entries.length;
-	if (li_entries_nr == 0) {
-		show_list_entries();
-	} else {
-		clear_list_entries();
-	}
-}
 
 // THIS WILL BE MOVED TO LOCALSTORAGE JAVASCRIPT LIBRARY!
 // function delete_entry(entrylist) { }
